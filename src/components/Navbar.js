cdaa92af65eaed6c85cfb56d1e6f1fef377c3667
jonav01 <shreyonjoneskol@gmail.com>
2022-10-14 10:33:18 +0530

    Navbar Layout Finished with basic hover animation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,15 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <ul className='navbarList'>
+                <div className='navbarList__leftItem'>
                 <li className="navbar__Items">Home</li>
                 <li className="navbar__Items">Blogs</li>
                 <li className="navbar__Items">Admin</li>
+                </div>
+                <div className='navbarList__rightItem'>
+                <li className="navbar__Items">Sign In</li>
+                <li className="navbar__Items">Sign Up</li>
+                </div>
             </ul>
         </nav>
     )
