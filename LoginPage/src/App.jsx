a8da46a4fe9b0b207eaed2f131f1cf8907190bd9
Sibayan Misra <sibayanmisra2002@gmail.com>
2022-10-18 23:55:36 +0530

    Add files via upload
    Files for the login page. 
    
    .JSX extension is used instead of .JS for APP.JSX file.

---
+++ LoginPage/src/App.jsx
@@ -0,0 +1,148 @@
+import styled from "styled-components";
+import Button from "./components/Button";
+import Icon from "./components/Icon";
+import Input from "./components/Input";
+import { FaFacebookF, FaGoogle, FaGithub } from "react-icons/fa";
+
+function App() {
+  const FacebookBackground =
+    "linear-gradient(to right, #0546A0 0%, #0546A0 40%, #663FB6 100%)";
+  const GoogleBackground =
+    "linear-gradient(to right, #A12AC4 0%, #ED586C 40%, #F0A853 100%)";
+  const GithubBackground =
+    "linear-gradient(to right, #56C1E1 0%, #35A9CE 50%)";
+  return (
+    <MainContainer>
+      <WelcomeText>Login Details</WelcomeText>
+      <InputContainer>
+        <Input type="text" placeholder="Username, email & phone number" />
+        <Input type="password" placeholder="Password" />
+      </InputContainer>
+      <ButtonContainer>
+        <Button content="Login" />
+      </ButtonContainer>
+      <LoginWith>Or Sign up With</LoginWith>
+      <HorizontalRule />
+      <IconsContainer>
+        <Icon color={GoogleBackground}>
+          <FaGoogle />
+        </Icon>
+        <Icon color={FacebookBackground}>
+          <FaFacebookF />
+        </Icon>
+        <Icon color={GithubBackground}>
+          <FaGithub />
+        </Icon>
+      </IconsContainer>
+      <ForgotPassword>Forgot Password ?</ForgotPassword>
+    </MainContainer>
+  );
+}
+
+const MainContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  height: 80vh;
+  width: 30vw;
+  background: rgba(255, 255, 255, 0.15);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(8.5px);
+  -webkit-backdrop-filter: blur(8.5px);
+  border-radius: 10px;
+  color: #ffffff;
+  text-transform: uppercase;
+  letter-spacing: 0.4rem;
+  @media only screen and (max-width: 320px) {
+    width: 80vw;
+    height: 90vh;
+    hr {
+      margin-bottom: 0.3rem;
+    }
+    h4 {
+      font-size: small;
+    }
+  }
+  @media only screen and (min-width: 360px) {
+    width: 80vw;
+    height: 80vh;
+    h4 {
+      font-size: small;
+    }
+    h2{
+      font-size: 1.2rem;
+    }
+  }
+  @media only screen and (min-width: 411px) {
+    width: 80vw;
+    height: 65vh;
+  }
+
+  @media only screen and (min-width: 768px) {
+    width: 50vw;
+    height: 70vh;
+  }
+  @media only screen and (min-width: 1024px) {
+    width: 40vw;
+    height: 80vh;
+  }
+  @media only screen and (min-width: 1280px) {
+    width: 30vw;
+    height: 80vh;
+  }
+`;
+
+const WelcomeText = styled.h2`
+  margin: 3rem 0 2rem 0;
+  color: #000000;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 20%;
+  width: 100%;
+  color: #000000;
+`;
+
+const ButtonContainer = styled.div`
+  margin: 1rem 0 2rem 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LoginWith = styled.h5`
+  cursor: pointer;
+  color: #000000;
+  letter-spacing: 0.1rem;
+`;
+
+const HorizontalRule = styled.hr`
+  width: 90%;
+  height: 0.3rem;
+  border-radius: 0.8rem;
+  border: none;
+  background: linear-gradient(to right, #14163c 0%, #03217b 79%);
+  background-color: #ebd0d0;
+  margin: 1.5rem 0 0.1rem 0;
+  backdrop-filter: blur(25px);
+`;
+
+const IconsContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin: 2rem 0 2rem 0;
+  width: 50%;
+`;
+
+const ForgotPassword = styled.h4`
+  cursor: pointer;
+  color: #000000;
+  letter-spacing: 0.2rem;
+`;
+
+export default App;
