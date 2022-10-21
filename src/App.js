62c52589a0834e9fa19ac6f2e01962b28d8c43fc
Shreya Prasad <shreyapprasad@gmail.com>
2022-10-21 22:30:10 +0530

    test push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login/Login";
+import Service from "./pages/About/Service";
 
 function App() {
   return (
     <div className="App">
       <Login />
       {/* <Navbar /> */}
+      {/* <Service /> */}
     </div>
   );
 }
