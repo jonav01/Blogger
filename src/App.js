5ee2d0c2829c9e30dd4efe6f36d3c344ba8b2dfb
Srijan Chatterjee <chatterjeer2001@gmail.com>
2022-10-28 21:43:48 +0530

    nav changed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import "./App.css";
 // import Navbar from "./components/Navbar";
 // import Login from "./pages/Login/Login";
+import Imagecarousel  from "./Imagecarousel";
 import About from "./pages/About/About";
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      {/* <Login /> */}
-      {/* <Navbar /> */}
-      <About />
+       {/* <Login />  */}
+       {/* <Navbar />  */}
+       <Navbar/>
+       <Imagecarousel />
+      
+      {/* <About /> */}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,18 @@
 import './Navbar.css' ;
+import React from 'react'
 
-const Navbar = () => {
+
+const Navbar= () => {
     return (
         <nav className="navbar">
             <ul className='navbarList'>
                 <div className='navbarList__leftItem'>
+                <li className="navbar__Items"><b>Your Blog</b></li>
                 <li className="navbar__Items">Home</li>
-                <li className="navbar__Items">Blogs</li>
-                <li className="navbar__Items">Admin</li>
+                <li className="navbar__Items">About us</li>
+                <li className="navbar__Items">Create</li>
                 </div>
-                <div className='navbarList__rightItem'>
-                <li className="navbar__Items">Sign In</li>
-                <li className="navbar__Items">Sign Up</li>
-                </div>
+               
             </ul>
         </nav>
     )
