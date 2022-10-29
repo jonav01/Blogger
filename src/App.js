9a20a35299357b5a57ee04cff870325236319178
vishaaal <officialvk02@gmail.com>
2022-10-29 21:03:46 +0530

    import About

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
-// import About from "./pages/About/About";
+import About from "./pages/About/About";
 import Blog from "./pages/Blog/Blog";
 import Write from "./pages/Write/Write";
 import Setting from "./pages/Setting/Setting";
@@ -17,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/register" element={ <Register />} />
+        <Route path="/about" element={ <About />} />
         <Route path="/login" element={ <Login />} />
         <Route path="/settings" element={user ? <Setting /> : <Register />} />
         <Route path="/write" element={user ? <Write /> : <Register />} />
