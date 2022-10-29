146a76fd584cd971e2f8e5b1f69ef5414f20eed8
Sibayan Misra <sibayanmisra2002@gmail.com>
2022-10-29 12:49:15 +0530

    Create blog test push

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 // import Navbar from "./components/Navbar";
 // import Login from "./pages/Login/Login";
-import About from "./pages/About/About";
+// import About from "./pages/About/About";
+import Write from "./pages/Write/write";
 
 function App() {
   return (
     <div className="App">
       {/* <Login /> */}
       {/* <Navbar /> */}
-      <About />
+      {/* <About /> */}
+      <Write />
     </div>
   );
 }
