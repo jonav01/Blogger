9bcaf6dc7f488cba1c3212eccb75ee9a8bd294a8
vishaaal <officialvk02@gmail.com>
2022-10-29 21:02:28 +0530

    [add] Link to Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Navbar.css' ;
 
 const Navbar= () => {
@@ -6,13 +7,14 @@ const Navbar= () => {
             <ul className='navbarList'>
                 <div className='navbarList__leftItem'>
                 <li className="navbar__Items"><i class="fa-solid fa-blog"></i>&nbsp;&nbsp;BLOGGER</li>
-                <li className="navbar__Items">Home</li>
-                <li className="navbar__Items">About</li>
+                <li className="navbar__Items"><Link to="/" style={{textDecoration:"none", color: "inherit"}}>Home</Link></li>
+                <li className="navbar__Items"><Link to="/about" style={{textDecoration:"none", color: "inherit"}}>About</Link></li>
                 </div>
                 <div className='navbarList__rightItem'>
-                <li className="navbar__Items">Write</li>
-                <li className="navbar__Items">Log Out</li>
-                <img src='https://cdn-icons-png.flaticon.com/512/2202/2202112.png' className='topImg'/>
+                <li className="navbar__Items"><Link to="/write" style={{textDecoration:"none", color: "inherit"}}>Write</Link></li>
+                <li className="navbar__Items"><Link to="/settings" style={{textDecoration:"none", color: "inherit"}}>Settings</Link></li>
+                <Link to="/login" style={{textDecoration:"none", color: "inherit"}}><img src='https://cdn-icons-png.flaticon.com/512/2202/2202112.png' className='topImg'/></Link>
+                
                 </div>
             </ul>
         </nav>
